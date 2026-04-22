beeda71ff8fc2403106b01acd9c5d578d6db17b2
Nathan Stitt <nathan@stitt.org>
2026-04-21 21:05:33 -0500

    refactor(mail): drop unnecessary type cast on useMailboxes

--- tinycld/mail/hooks/useMailboxes.ts
+++ tinycld/mail/hooks/useMailboxes.ts
@@ -2,7 +2,6 @@ import { eq } from '@tanstack/db'
 import { useMemo } from 'react'
 import { useStore } from '@tinycld/core/lib/pocketbase'
 import { useOrgLiveQuery } from '@tinycld/core/lib/use-org-live-query'
-import type { MailMailboxes } from '../types'
 import { splitMailboxes } from './splitMailboxes'
 
 export { splitMailboxes }
@@ -26,6 +25,6 @@ export function useMailboxes() {
 
     return useMemo(() => {
         const ids = (members ?? []).map((m) => m.mailbox)
-        return splitMailboxes(ids, (mailboxes ?? []) as MailMailboxes[])
+        return splitMailboxes(ids, mailboxes ?? [])
     }, [members, mailboxes])
 }
