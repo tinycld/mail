7c3f3ca0cb24d953616311e3ad52088e2205811e
Nathan Stitt <nathan@stitt.org>
2026-09-17 15:48:36 -0500

    chore(mail): require @tinycld/core >=0.5.1

--- manifest.ts
+++ manifest.ts
@@ -57,7 +57,7 @@ const manifest = {
         module: 'tinycld.org/packages/mail/cli',
     },
     repository: { url: 'https://github.com/tinycld/mail' },
-    peerVersions: { '@tinycld/core': '>=0.1.0 <0.2.0' },
+    peerVersions: { '@tinycld/core': '>=0.5.1 <0.6.0' },
 }
 
 export default manifest
